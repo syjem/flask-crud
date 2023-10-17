1b61bfd442855e0789809c45709f38af9dddfdd8
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-10-17 16:40:45 +0800

    delete models.py file

--- src/models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Birthdays(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f"Name: ('{self.name}', Birthday: '{self.date}')"
