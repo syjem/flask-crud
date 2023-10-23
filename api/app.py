1ea505c6b6e8051fe1f37d3ad1c05b00fa82fe7e
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-10-23 14:26:01 +0800

    revert

--- api/app.py
+++ api/app.py
@@ -1,6 +1,35 @@
-from flask import jsonify, request
-from api import app, db
-from api.models import Birthdays
+from flask_cors import CORS 
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+
+from config import Config
+
+app = Flask(__name__)
+CORS(app)
+
+# Configs
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+
+class Birthdays(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"Name: ('{self.name}', Birthday: '{self.date}')"
+
+with app.app_context():
+    db.create_all()
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/birthdays', methods=["GET", "POST", "DELETE", "PATCH"])
 def index():
@@ -56,3 +85,6 @@ def index():
     birthdays = Birthdays.query.all()
     birthday_data = [{'id': birthday.id, "name": birthday.name, "date": birthday.date} for birthday in birthdays]
     return jsonify(birthday_data)
+
+if __name__ == ("__main__"):
+    app.run(debug=True)
